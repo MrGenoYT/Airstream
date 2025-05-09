2d783590bdc1783ca78ac73d3ddfae5d8a66210c
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-10 00:31:53 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-750': '#2d3748',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      height: {
+        'screen-90': '90vh',
+      },
+    },
   },
   plugins: [],
 }
