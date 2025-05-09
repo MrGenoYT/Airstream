7634e54c940fc7e064f98944182750032ce5f890
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-09 21:49:40 +0530

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className="text-center mt-4">
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-white mx-auto" />
+      <p className="mt-2">Loading...</p>
+    </div>
+  )
+    }
