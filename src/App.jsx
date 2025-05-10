8dcf9470b0797f65cfa1a61d0195128d28695865
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-05-10 11:11:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -156,7 +156,7 @@ function App() {
                     <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <h3 className="font-bold text-white">FAST DOWNLOADS</h3>
+                <h3 className="font-bold text-white">FAST</h3>
               </div>
               <div className="flex flex-col items-center">
                 <div className="bg-blue-900/30 p-4 rounded-full mb-3 border border-blue-800">
